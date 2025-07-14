5284007693582095352dd57dd3dcb3649a5df14a
Owais-Siddique-11 <siddiqueowais499@gmail.com>
2025-07-14 14:29:26 +0530

    First Commit added Setup the basic project and User Model

---
+++ src/repository/user-repository.js
@@ -0,0 +1,16 @@
+const {User} = require('../models/user');
+
+class UserRepository {
+    async createUser(data){
+        return await User.create(data);
+    }
+    async findUserByEmail(email){
+        return await User.findOne({
+            where : {
+                email
+            }
+        });
+    }
+
+}
+module.exports= UserRepository;
