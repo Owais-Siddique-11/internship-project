5a100d8617d9fa5a0e22d6197590f86eead75907
Owais-Siddique-11 <siddiqueowais499@gmail.com>
2025-07-29 18:10:26 +0530

    Added some code to index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,16 +1,20 @@
-const express= require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const { PORT } = require('./src/config/serverConfig');
-const routes = require('./src/routes/index');
-const setupAndStartServer = async ()=>{
-    const app= express();
-    app.use(cors());
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended : true}));
-    app.use('/api',routes);
-    app.listen(PORT,()=>{
-        console.log(`Server started at port ${PORT}`);
-    });
-}
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { PORT } = require("./src/config/serverConfig");
+const routes = require("./src/routes/index");
+const setupAndStartServer = async () => {
+  const app = express();
+  app.get("/", (req, res) => {
+    res.send("🎉 Backend is Live!");
+  });
+
+  app.use(cors());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use("/api", routes);
+  app.listen(PORT, () => {
+    console.log(`Server started at port ${PORT}`);
+  });
+};
 setupAndStartServer();
